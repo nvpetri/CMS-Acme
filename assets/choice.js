23a1bd8b28ccc7734c2c9b16cd73d0ef2932d166
nvpetri <nicolaspetri18@gmail.com>
2024-04-10 15:20:22 -0300

    feature: tela de escolhas

---
+++ assets/choice.js
@@ -0,0 +1,7 @@
+'use strict'
+
+const btnFilmes = document.getElementById('filme')
+
+btnFilmes.addEventListener('click', () => {
+    window.location.href = '../pages/home.html'
+})
--- assets/login.js
+++ assets/login.js
@@ -6,7 +6,7 @@ const button = document.getElementById('btn-login')
 
 function validarUsuario() {
     if (mail.value == 'nicolas' && pass.value == '1234') {
-        window.location.href = './pages/home.html'
+        window.location.href = './pages/choice.html'
     }
 }
 
